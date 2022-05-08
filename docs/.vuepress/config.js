97ec762df21f316b9604e85cba1e7038a5102a35
Joe Chen <jc@unknwon.io>
2022-05-08 14:22:29 +0800

    配合 VuePress 新版配置语法

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,5 @@
+const { defaultTheme } = require("@vuepress/theme-default");
+
 module.exports = {
     title: "Flamego",
     description: "一款简洁的、极易扩展且模块化的 Go Web 框架",
@@ -6,7 +8,10 @@ module.exports = {
         ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
         [
             "meta",
-            { name: "apple-mobile-web-app-status-bar-style", content: "black" },
+            {
+                name: "apple-mobile-web-app-status-bar-style",
+                content: "black",
+            },
         ],
         ["link", { rel: "icon", href: "/favicon.png" }],
         [
@@ -25,62 +30,58 @@ module.exports = {
         ],
     ],
 
-    themeConfig: {
-        locales: {
-            "/": {
-                navbar: [
-                    {
-                        text: "初学指南",
-                        link: "/starter-guide.html",
-                    },
-                    {
-                        text: "开发手册",
-                        children: [
-                            "/core-concepts.html",
-                            "/core-services.html",
-                            "/custom-services.html",
-                            "/routing.html",
-                        ],
-                    },
-                    {
-                        text: "中间件集成",
-                        link: "/middleware/",
-                    },
-                    {
-                        text: "常见问题",
-                        link: "/faqs.html",
-                    },
-                    {
-                        text: "视频教程",
-                        link: "https://github.com/unknwon/flamego-basics",
-                    },
-                ],
-
-                // page meta
-                editLinkText: "在 GitHub 上编辑此页",
-                lastUpdatedText: "上次更新",
-                contributorsText: "贡献者",
-
-                // custom containers
-                tip: "提示",
-                warning: "注意",
-                danger: "警告",
-
-                // 404 page
-                notFound: [
-                    "这里什么都没有",
-                    "我们怎么到这来了？",
-                    "这是一个 404 页面",
-                    "看起来我们进入了错误的链接",
-                ],
-                backToHome: "返回首页",
-
-                // a11y
-                openInNewWindow: "在新窗口打开",
-                toggleDarkMode: "切换夜间模式",
-                toggleSidebar: "切换侧边栏",
+    theme: defaultTheme({
+        navbar: [
+            {
+                text: "初学指南",
+                link: "/starter-guide.html",
             },
-        },
+            {
+                text: "开发手册",
+                children: [
+                    "/core-concepts.html",
+                    "/core-services.html",
+                    "/custom-services.html",
+                    "/routing.html",
+                ],
+            },
+            {
+                text: "中间件集成",
+                link: "/middleware/",
+            },
+            {
+                text: "常见问题",
+                link: "/faqs.html",
+            },
+            {
+                text: "视频教程",
+                link: "https://github.com/unknwon/flamego-basics",
+            },
+        ],
+
+        // page meta
+        editLinkText: "在 GitHub 上编辑此页",
+        lastUpdatedText: "上次更新",
+        contributorsText: "贡献者",
+
+        // custom containers
+        tip: "提示",
+        warning: "注意",
+        danger: "警告",
+
+        // 404 page
+        notFound: [
+            "这里什么都没有",
+            "我们怎么到这来了？",
+            "这是一个 404 页面",
+            "看起来我们进入了错误的链接",
+        ],
+        backToHome: "返回首页",
+
+        // a11y
+        openInNewWindow: "在新窗口打开",
+        toggleDarkMode: "切换夜间模式",
+        toggleSidebar: "切换侧边栏",
         logo: "/favicon.png",
         repo: "flamego/flamego",
         repoLabel: "GitHub",
@@ -90,5 +91,5 @@ module.exports = {
         docsDir: "docs",
         lastUpdated: true,
         contributors: false,
-    },
+    }),
 };
